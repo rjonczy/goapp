0a6f85f6c2ccae116e159b7bb59d135d6654dfd3
Robert Jonczy <robert.jonczy@gmail.com>
2024-02-01 22:39:14 +0100

    make it a server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rjonczy/goapp
 
 go 1.21.4
+
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := mux.NewRouter()
+	r.HandleFunc("/", homeHandler)
+	http.Handle("/", r)
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
